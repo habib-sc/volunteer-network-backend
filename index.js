4d918f44ac926752aff0d9807f41f869f2b25408
habib-sc <habib97.cs@gmail.com>
2022-04-28 02:52:28 +0600

    DB connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -12,6 +13,25 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Volunteer Network Server');
 });
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteernetwork.dv1mu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        await client.connect();
+        console.log('Database Connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.listen(port, () => {
