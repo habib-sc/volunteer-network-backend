6f48886ce20ca05da111a13834558fbcd99dab71
habib-sc <habib97.cs@gmail.com>
2022-04-28 20:17:22 +0600

    Added user detail endpoint

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ async function run () {
             res.send(result);
         });
 
+
+        // User detail endpoint -- http://localhost:5000/user/email
+        app.get('/user/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+        });
+
         // Event Update endpoint -- http://localhost:5000/event/edit/id
         app.put('/event/edit/:id', async(req, res) => {
             const id = req.params.id;
